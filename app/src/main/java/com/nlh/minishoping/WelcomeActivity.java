7a4151a87b12d4e4fd85ecd644b625d8c13d4413
mekanican <boommerang73@gmail.com>
2022-11-28 17:28:04 +0700

    Done splash

--- app/src/main/java/com/nlh/minishoping/WelcomeActivity.java
+++ app/src/main/java/com/nlh/minishoping/WelcomeActivity.java
@@ -1,28 +1,19 @@
 package com.nlh.minishoping;
 
-import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.constraintlayout.widget.ConstraintLayout;
 
 import android.content.Intent;
 import android.content.res.AssetManager;
-import android.graphics.Bitmap;
-import android.graphics.BitmapFactory;
-import android.graphics.drawable.BitmapDrawable;
 import android.os.Bundle;
-import android.util.Log;
-import android.widget.Button;
 
 import java.util.ArrayList;
 
 public class WelcomeActivity extends AppCompatActivity {
     public static AssetManager assetManager;
 
-    // Hello Loc was here
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_welcome);
 
         assetManager = getAssets();
 
@@ -32,26 +23,8 @@ public class WelcomeActivity extends AppCompatActivity {
         for (int i = 0; i < 20; i += 2) {
             SharedInfo.getInstance().addProductToCart(t.get(i));
         }
-
-        // Hide the "ActionBar" in this activity
-        ActionBar actionBar = getSupportActionBar();
-        actionBar.hide();
-
-        // Manually Blurred background
-        ConstraintLayout constraintLayout = findViewById(R.id.init_page);
-        Bitmap image = BitmapFactory.decodeResource(getResources(),
-                R.drawable.background);
-        Bitmap background_blur = BlurBuilder.blur(this, image);
-        constraintLayout.setBackground(
-                new BitmapDrawable(getResources(), background_blur)
-        );
-        // Button to switch activity (e.g. Cart activity)
-        Button button = findViewById(R.id.button);
-        button.setOnClickListener(view -> {
-            Intent intent = new Intent(this, MainActivity.class);
-            startActivity(intent);
-        });
-
-
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
+        finish();
     }
 }

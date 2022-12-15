7821b5b0821d337746b45d80158ff5bada9d67b0
mekanican <boommerang73@gmail.com>
2022-12-16 00:33:09 +0700

    (Maybe) Account chooser

--- app/src/main/java/com/nlh/minishoping/WelcomeActivity.java
+++ app/src/main/java/com/nlh/minishoping/WelcomeActivity.java
@@ -2,6 +2,8 @@ package com.nlh.minishoping;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.accounts.Account;
+import android.accounts.AccountManager;
 import android.content.Intent;
 import android.content.res.AssetManager;
 import android.os.Bundle;
@@ -19,7 +21,19 @@ public class WelcomeActivity extends AppCompatActivity {
 
         // Init data before starting!
         SharedInfo.getInstance().initData();
-        Intent intent = new Intent(this, MainActivity.class);
+
+        // https://developer.android.com/training/id-auth/identify
+        AccountManager am = AccountManager.get(this); // "this" references the current Context
+        Account[] accounts = am.getAccountsByType("com.google");
+
+        // https://stackoverflow.com/questions/15406535/accountmanager-how-to-let-the-user-select-an-account-using-a-dialog
+        Intent intent = AccountManager.newChooseAccountIntent(null, null,
+                new String[]{"com.google"}, null, null, null,
+                null);
+
+        startActivityForResult(intent, 1);
+
+        intent = new Intent(this, MainActivity.class);
         startActivity(intent);
         finish();
     }

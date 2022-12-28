79d814c041614687243f4580f3d0b68523beeef9
vanloc1808 <vanloc1808@gmail.com>
2022-12-28 16:43:58 +0700

    add notifcation pop-up

--- app/src/main/java/com/nlh/minishoping/MainActivity.java
+++ app/src/main/java/com/nlh/minishoping/MainActivity.java
@@ -1,6 +1,7 @@
 package com.nlh.minishoping;
 
-import static com.nlh.minishoping.NotificationClass.CHANNEL_ID;
+import static com.nlh.minishoping.NotificationClass.CHANNEL_1;
+import static com.nlh.minishoping.NotificationClass.CHANNEL_2;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.NotificationCompat;
@@ -8,7 +9,10 @@ import androidx.fragment.app.Fragment;
 
 import android.app.Notification;
 import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.app.TaskStackBuilder;
 import android.content.Context;
+import android.content.Intent;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.os.Bundle;
@@ -20,7 +24,9 @@ import com.nlh.minishoping.Cart.CartMap;
 
 public class MainActivity extends AppCompatActivity {
 
-    private static final int NOTIFICATION_ID = 1;
+    private static final int WELCOME_NOTIFICATION_ID = 1;
+    private static final int DISCOUNT_NOTIFICATION_ID = 2;
+
     Fragment currentFragment;
     CartMap cartMap;
 
@@ -91,23 +97,54 @@ public class MainActivity extends AppCompatActivity {
         currentFragment = storeFragment;
 
         sendWelcomeNotification();
+
+        sendDiscountNotification();
     }
 
     private void sendWelcomeNotification() {
         Bitmap bitmap = BitmapFactory.decodeResource(getResources(), R.drawable.ic_baseline_notifications_24);
 
-        Notification notification = new NotificationCompat.Builder(this, CHANNEL_ID)
-                .setContentTitle("Welcome to MiniShopping")
-                .setContentText("Hope you will enjoy this app!! Sure")
+        Notification notification = new NotificationCompat.Builder(this, CHANNEL_1)
+                .setContentTitle(getString(R.string.welcome_noti_title))
+                .setContentText(getString(R.string.welcome_noti_content))
                 .setSmallIcon(R.drawable.ic_baseline_notifications_24)
                 .setLargeIcon(bitmap)
                 .build();
 
         NotificationManager notificationManager = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
         if (notificationManager != null) {
-            notificationManager.notify(NOTIFICATION_ID, notification);
+            notificationManager.notify(WELCOME_NOTIFICATION_ID, notification);
         }
     }
 
 
+    private void sendDiscountNotification() {
+        Bitmap bitmap = BitmapFactory.decodeResource(getResources(), R.drawable.ic_baseline_notifications_24);
+
+        // Create an Intent for the activity you want to start
+        Intent intent = new Intent(this, ProductDetails.class);
+        intent.putExtra("ID", 2);
+
+        // Create the TaskStackBuilder and add the intent, which inflates the back stack
+        TaskStackBuilder stackBuilder = TaskStackBuilder.create(this);
+        stackBuilder.addNextIntentWithParentStack(intent);
+
+        // Get the PendingIntent containing the entire back stack
+        PendingIntent resultPendingIntent =
+                stackBuilder.getPendingIntent(0,
+                        PendingIntent.FLAG_UPDATE_CURRENT | PendingIntent.FLAG_IMMUTABLE);
+
+        Notification notification = new NotificationCompat.Builder(this, CHANNEL_2)
+                .setContentTitle("Sản phẩm hot giảm sâu!")
+                .setContentText("Bạn ơi, sản phẩm hot đang giảm sâu nè. Xem ngay thôi!")
+                .setSmallIcon(R.drawable.ic_baseline_notifications_24)
+                .setLargeIcon(bitmap)
+                .setContentIntent(resultPendingIntent)
+                .build();
+
+        NotificationManager notificationManager = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+        if (notificationManager != null) {
+            notificationManager.notify(DISCOUNT_NOTIFICATION_ID, notification);
+        }
+    }
 }
--- app/src/main/java/com/nlh/minishoping/NotificationClass.java
+++ app/src/main/java/com/nlh/minishoping/NotificationClass.java
@@ -5,7 +5,8 @@ import android.app.NotificationChannel;
 import android.app.NotificationManager;
 
 public class NotificationClass extends Application {
-    public static final String CHANNEL_ID = "CHANNEL1";
+    public static final String CHANNEL_1 = "CHANNEL1";
+    public static final String CHANNEL_2 = "CHANNEL2";
 
     @Override
     public void onCreate() {
@@ -17,17 +18,23 @@ public class NotificationClass extends Application {
     private void createNotificationChannel() {
         // Create the NotificationChannel, but only on API 26+ because
         // the NotificationChannel class is new and not in the support library
-        CharSequence name = getString(R.string.channel_name);
-        String description = getString(R.string.channel_description);
+        CharSequence name1 = getString(R.string.channel1_name);
+        String description1 = getString(R.string.channel1_description);
+        CharSequence name2 = getString(R.string.channel2_name);
+        String description2 = getString(R.string.channel2_description);
         int importance = NotificationManager.IMPORTANCE_DEFAULT;
-        NotificationChannel channel = new NotificationChannel(CHANNEL_ID, name, importance);
-        channel.setDescription(description);
+        NotificationChannel channel1 = new NotificationChannel(CHANNEL_1, name1, importance);
+        channel1.setDescription(description1);
+
+        NotificationChannel channel2 = new NotificationChannel(CHANNEL_2, name2, importance);
+        channel2.setDescription(description2);
 
         // Register the channel with the system; you can't change the importance
         // or other notification behaviors after this
         NotificationManager notificationManager = getSystemService(NotificationManager.class);
         if (notificationManager != null) {
-            notificationManager.createNotificationChannel(channel);
+            notificationManager.createNotificationChannel(channel1);
+            notificationManager.createNotificationChannel(channel2);
         }
     }
 }

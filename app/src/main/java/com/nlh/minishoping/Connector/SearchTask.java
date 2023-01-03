7d6b15699728b09fc136abbdda37e1c9f8e24e76
vanloc1808 <vanloc1808@gmail.com>
2023-01-03 11:07:46 +0700

    add get search results from server

---
+++ app/src/main/java/com/nlh/minishoping/Connector/SearchTask.java
@@ -0,0 +1,53 @@
+package com.nlh.minishoping.Connector;
+
+import static com.nlh.minishoping.Connector.ServerConnector.API_PATH;
+import static com.nlh.minishoping.Connector.ServerConnector.HOST_NAME;
+
+import android.os.AsyncTask;
+import android.util.Log;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+
+public class SearchTask extends AsyncTask<String, Void, String> {
+    protected String doInBackground(String... searchTerms) {
+        String searchURL = HOST_NAME + API_PATH + "product?type=search&query=" + searchTerms[0];
+        Log.i("SEARCH THIS", searchURL);
+
+        try {
+            URL url = new URL(searchURL);
+            Log.i("URL", url.toString());
+            HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+
+            int responseCode = conn.getResponseCode();
+
+            if (responseCode == HttpURLConnection.HTTP_OK) {
+                // get the response body
+                BufferedReader in = new BufferedReader(new InputStreamReader(conn.getInputStream()));
+
+                String inputLine;
+                StringBuilder response = new StringBuilder();
+
+                while ((inputLine = in.readLine()) != null) {
+                    response.append(inputLine);
+                }
+
+                in.close();
+
+                Log.i("RESPONSE", String.valueOf(response));
+
+                return response.toString();
+            }
+
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+}
